416a24dde4f5fe0f5712ca93df0cabfaa7f075b9
msdone96 <msdone96@gmail.com>
2024-12-11 14:50:24 +0530

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,8 +1,17 @@
-from tasks.task_runner import run_task
+import time
+from tasks.task_manager import fetch_and_process_data
 
+# Main function to run the scraper
 def main():
-    print("Starting the project...")
-    run_task()
+    print("Starting Flipkart Scraper...")
+
+    # Call the function to fetch and process data
+    try:
+        fetch_and_process_data()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    print("Scraping completed.")
 
 if __name__ == "__main__":
     main()
